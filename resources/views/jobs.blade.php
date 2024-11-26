724ba227eb97dc73ebfb418dcd803dbdf67228a1
Pernilla Sterner <psterner@hotmail.com>
2024-11-26 20:06:34 +0100

    View data and Route Wildcards

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,7 +6,7 @@
     <ul>
         @foreach ($jobs as $job)
             <li>
-                <a href="/job/{{ $job['id'] }}">
+                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                 </a>
             </li>
