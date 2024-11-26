a912a59cd73fd3d68ff84bf7a91c6db5aa2dddee
Pernilla Sterner <psterner@hotmail.com>
2024-11-26 19:55:28 +0100

    Search for job with matching id

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,8 +10,54 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Directory',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ]
+         ]
+    ]
+);
+});
+
+// Give me the job that is equal to the id that was passed in
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Directory',
+            'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$10,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$40,000'
+        ]
+            ];
+
+    // search for the job with the matching id
+    $job = Arr::first($jobs, fn($job) => ($job['id']) == $id);
+            dd($job); // jobs/3
+    return view('job', ['job' => $job]);
+
 });
 
 Route::get('/contact', function () {
