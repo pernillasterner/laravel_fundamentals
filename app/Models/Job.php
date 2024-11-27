d8fc315f055e1d7236098d1d8ec1d5c7237024ed
Pernilla Sterner <psterner@hotmail.com>
2024-11-27 15:22:41 +0100

    Create relations between jobs and employers. An employer hasMany jobs

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -14,4 +14,10 @@ class Job extends Model {
 
     protected $fillable = ['title', 'salary'];
 
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
+
+    
+
 }
