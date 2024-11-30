aff9e62c6cc614bce6bc2ded83bba563dfc0aa7f
Pernilla Sterner <psterner@hotmail.com>
2024-11-30 14:06:19 +0100

    Add pagination for job listings

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,12 +5,17 @@
 
     <div class="space-y-4">
         @foreach ($jobs as $job)
-            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-grey-200 rounded-lg">
-                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border rounded-lg border-grey-200">
+                <div class="text-sm font-bold text-blue-500">{{ $job->employer->name }}</div>
                 <div>
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                 </div>
             </a>
         @endforeach
+
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
     </div>
 </x-layout>
