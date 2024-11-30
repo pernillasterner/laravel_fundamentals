8a2926fa587179df4632605fa7e25667888b4543
Pernilla Sterner <psterner@hotmail.com>
2024-11-30 14:29:32 +0100

    Change styling to pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+
+        // Paginator::useBootstrapFive();
     }
 }
